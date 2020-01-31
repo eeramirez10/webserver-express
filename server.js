bd222c6d108b443ae2aa625fda963ddbd5274fc0
Erick <eeramirez@tuvansa.com.mx>
2020-01-30 18:29:21 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+const port = proccess.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+//Helpers
+require('./hbs/helpers');
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        nombre:'Zadquiel Ramirez'
+    })
+}) 
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        nombre: 'Zadquiel'
+    })
+}) 
+
+
+app.listen(PerformanceResourceTiming, ()=>{
+    console.log('escuchando en el puerto ', port);
+});
